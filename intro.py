4c2581b18a9942e83f8ee1be9ca04f9267358e2d
JineeLamp <gnana7720@gmail.com>
2024-08-30 19:31:47 +0900

    Add files via upload

--- intro.py
+++ intro.py
@@ -3,12 +3,12 @@ import streamlit as st
 def intro_main():
     st.image('pendulum.jpg', width=200)
     st.write("이 포트폴리오는") 
-    st.write("2024년 6월18일부터 8월29일까지 '강북여성새로일하기센터'에서 개최한 'AI LITERACY 전문가 과정'에서 진행한 결과의 일부를 모은 것입니다.")
-    st.write("파이썬 기초는 파이썬의 간단한 연산기능을 다루었고,")
-    st.write("데이터 분석은 파이썬의 pandas, numpy, matplotlib, seaborn을 활용하여 수행했으며,")
-    st.write("머신러닝은 파이썬의 scikit-learn을 활용하여 RandomForest 모형으로 예측하였습니다.")
-    st.write("미니프로젝트는 강북여성새일터가 있는 수유역 근처의 혼밥 맛집을 '예산'에 맞게 추천하는 프로그램입니다.")
-    st.write("최종 포트폴리오는 github과 streamlit 앱을 활용한 것입니다.")
+    st.write("2024년 6월18일부터 8월29일까지 '강북여성새로일하기센터'에서 개최한 'AI LITERACY 전문가 과정'에 참여하여 진행한 결과의 일부를 모은 것입니다.")
+    st.write("'파이썬 기초'는 파이썬의 간단한 연산기능을 다루었고,")
+    st.write("'탐색적 분석'은 파이썬의 pandas, numpy, matplotlib, seaborn을 활용하여 수행했으며,")
+    st.write("'머신러닝'은 파이썬의 scikit-learn을 활용하여 RandomForest 모형으로 예측한 결과입니다.")
+    st.write("'미니프로젝트'는 강북여성새일터가 자리한 수유역 주변의 혼밥 맛집을 '예산'에 맞게 추천하는 프로그램입니다.")
+    st.write("포트폴리오 앱은 github과 streamlit을 활용한 것입니다.")
     st.write("감사합니다.")
     st.write("***Jinee***")
     
